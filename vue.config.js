6e0421cc7c5b704280d574dd006c181370edf932
Timur <targon_k_p@mail.ru>
2025-10-02 00:13:47 +0300

    deploy for Github Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/forecast-weather-project/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
